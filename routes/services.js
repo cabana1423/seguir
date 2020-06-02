234e2752d14832564795486520159740f5fa0984
cabana <cabanaxsiempre@gmail.com>
2020-06-02 02:53:23 -0400

    creacion de un servicio POST

--- routes/services.js
+++ routes/services.js
@@ -1,10 +1,21 @@
 var express = require('express'); 
 var router = express.Router(); 
 /* GET home page. */ 
-router.get('/', function(req, res, next) { 
+router.get('/usuario', function(req, res, next) { 
+  var datos =req.query;
+  var name=datos.name;
+  console.log(datos);
+  console.log(name);
   res.status(200).json({ 
     msn: "Hola mundo" 
   }); 
+});
+router.post('/test', (req, res, next)=> { 
+  req.body["msn"] = "Por el servidor"; 
+  var data = req.body  
+  res.status(200).json(data); 
 }); 
-module.exports = router; 
+
+module.exports = router;
+
 
